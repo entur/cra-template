183b231cf7fa3672d15682732f9b8516e7e4b851
Mats Byrkjeland <matsbyr@gmail.com>
2021-02-02 16:38:02 +0100

    Add Prettier to this repo and template for formatting

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,60 +1,72 @@
-import React from 'react';
+import React from 'react'
 
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useLocation,
-} from "react-router-dom";
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useLocation,
+} from 'react-router-dom'
 
-import { SideNavigation, SideNavigationItem } from '@entur/menu';
+import { SideNavigation, SideNavigationItem } from '@entur/menu'
 
-import Help from './pages/Help';
-import Home from './pages/Home';
-import Info from './pages/Info';
+import Help from './pages/Help'
+import Home from './pages/Home'
+import Info from './pages/Info'
 
-import './App.css';
+import './App.css'
 
 const Menu = () => {
-  const location = useLocation()
+    const location = useLocation()
 
-  return (
-    <SideNavigation>
-      <SideNavigationItem as={Link} to="/" active={location.pathname === '/'}>
-        Hjem
-      </SideNavigationItem>
-      <SideNavigationItem as={Link} to="/info" active={location.pathname === '/info'}>
-        Informasjon
-      </SideNavigationItem>
-      <SideNavigationItem as={Link} to="/hjelp" active={location.pathname === '/hjelp'}>
-        Hjelp
-      </SideNavigationItem>
-    </SideNavigation>
-  )
+    return (
+        <SideNavigation>
+            <SideNavigationItem
+                as={Link}
+                to="/"
+                active={location.pathname === '/'}
+            >
+                Hjem
+            </SideNavigationItem>
+            <SideNavigationItem
+                as={Link}
+                to="/info"
+                active={location.pathname === '/info'}
+            >
+                Informasjon
+            </SideNavigationItem>
+            <SideNavigationItem
+                as={Link}
+                to="/hjelp"
+                active={location.pathname === '/hjelp'}
+            >
+                Hjelp
+            </SideNavigationItem>
+        </SideNavigation>
+    )
 }
 
 function App() {
-  return (
-      <Router>
-        <div className="app">
-          <Menu />
-          <div className="app-wrapper">
-            <Switch>
-              <Route path="/info">
-                <Info />
-              </Route>
-              <Route path="/hjelp">
-                <Help />
-              </Route>
-              <Route path="/">
-                <Home />
-              </Route>
-            </Switch>
-          </div>
-        </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="app">
+                <Menu />
+                <div className="app-wrapper">
+                    <Switch>
+                        <Route path="/info">
+                            <Info />
+                        </Route>
+                        <Route path="/hjelp">
+                            <Help />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    )
 }
 
-export default App;
+export default App
